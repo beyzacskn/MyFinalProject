97c6c4674dd4df23216b872dcd8b3d1c9b859319
Beyza Coşkun <beyza.coskun025@gmail.com>
2021-01-30 23:04:29 +0300

    adding

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity //diğer classların ulaşabilmesi için
+    {
+        public int ProductId { get; set; }    
+        public  int CategoryId { get; set; } 
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+}
+}
